0c2aa0ba4cdae6df0a629a9be66fe721415dcf2d
Colin Cross <ccross@android.com>
2017-06-26 10:18:34 -0700

    Compile darwin with prebuilt clang
    The mac buildbots have an old version of clang, use the platform
    prebuilt clang instead.
    
    Change-Id: Icbf5f8c8fd7750233a0d648bf354e6dc109948ac

--- build-openjdk-darwin.sh
+++ build-openjdk-darwin.sh
@@ -4,29 +4,57 @@ set -e
 
 source $(dirname $0)/build-openjdk-common.sh
 
-sysroot=${OUT}/sysroot
-tools_dir=${TOP}/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8/bin
 freetype=freetype-2.8
+clang_dir=${TOP}/prebuilts/clang/host/darwin-x86/clang-stable
+sdk_version=$(xcrun --show-sdk-version)
+if [ "${sdk_version}" != "10.9" -a "${sdk_version}" != "10.10" ]; then
+    echo "Xcode sdk version 10.9 or 10.10 is required"
+    exit 1
+fi
+sysroot=$(xcrun --show-sdk-path)
+
+export PATH=${clang_dir}/bin:${PATH}
 
 (
   cd ${OUT}
   tar jxf ${TOP}/toolchain/jdk/deps/src/${freetype}.tar.bz2
   (
     cd ${freetype}
-    ./configure
-    make -j8
+    bash configure \
+      CC=clang \
+      CC_BUILD=clang \
+      LD=clang \
+      CFLAGS="-isysroot ${sysroot}" \
+    && make -j8 CCexe_CFLAGS="-isysroot ${sysroot}"
     mkdir -p lib
     cp objs/.libs/libfreetype.dylib lib/
   )
 
   export MACOSX_DEPLOYMENT_TARGET=10.8
   bash configure \
+    --host=x86_64-apple-darwin \
     --with-boot-jdk=${TOP}/prebuilts/studio/jdk/mac/Contents/Home \
     --with-milestone=android \
     --with-update-version=${JDK_UPDATE_VERSION} \
     --with-build-number=${JDK_BUILD_NUMBER} \
     --with-user-release-suffix=${BUILD_NUMBER} \
     --with-freetype=${OUT}/${freetype} \
+    --with-extra-cflags="-isysroot ${sysroot}" \
+    --with-extra-cxxflags="-isysroot ${sysroot}" \
+    LDFLAGS="-isysroot ${sysroot}" \
+    CFLAGS="-isysroot ${sysroot}" \
+    CPPFLAGS="-isysroot ${sysroot}" \
+    CXXFLAGS="-isysroot ${sysroot}" \
+    X_CFLAGS="-isysroot ${sysroot}" \
+    CCXXFLAGS_JDK="-isysroot ${sysroot}" \
+    LDFLAGS_JDK="-isysroot ${sysroot}" \
+    CC="clang -isysroot ${sysroot}" \
+    CXX="clang++ -isysroot ${sysroot}" \
+    LD="clang -isysroot ${sysroot}" \
+    BUILD_CC="${clang_dir}/bin/clang -isysroot ${sysroot}" \
+    BUILD_CXX="${clang_dir}/bin/clang++ -isysroot ${sysroot}" \
+    BUILD_LD="${clang_dir}/bin/clang -isysroot ${sysroot}" \
+    ZIP="/usr/bin/zip -X" \
   && make images VERBOSE= COMPILER_WARNINGS_FATAL=false
 )
 
