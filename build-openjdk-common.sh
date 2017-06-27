47c015dded22d15938388fd5f547c2302cdd016e
Colin Cross <ccross@android.com>
2017-06-27 12:44:43 -0700

    Refactor more code into build-openjdk-common.sh
    Now that mac is using a prebuilt toolchain too, most of the
    configure and build logic is similar between mac and linux.
    
    Test: builds on mac and linux
    Change-Id: I027b129506e3a7bde5cbc96869da7da0a5802387

--- build-openjdk-common.sh
+++ build-openjdk-common.sh
@@ -1,10 +1,12 @@
+set -e
+set -x
 
 JDK_UPDATE_VERSION=152
 JDK_BUILD_NUMBER=1
 
 if [ -z "${OUT_DIR}" ]; then
-    echo "OUT_DIR must be set"
-    exit 1
+  echo "OUT_DIR must be set"
+  exit 1
 fi
 
 mkdir -p ${OUT_DIR}
@@ -13,28 +15,99 @@ TOP=$(cd $(dirname $0)/../../.. && pwd)
 JDK_SRC=${TOP}/external/jetbrains
 
 case $(uname) in
-    Linux)
-        BUILD_TOOLS=${TOP}/prebuilts/build-tools/linux-x86/bin
-        ;;
-    Darwin)
-        BUILD_TOOLS=${TOP}/prebuilts/build-tools/darwin-x86/bin
-        ;;
-    *) echo "unknown OS:" $(uname) && exit 1;;
+  Linux)
+    PREBUILT_DIR=linux-x86
+    TRIPLE=x86_64-linux-gnu
+    JAVA_PREBUILT_SUBDIR=linux
+    OPENJDK_IMAGES_SUBDIR=linux-x86_64-normal-server-release
+    ;;
+  Darwin)
+    PREBUILT_DIR=darwin-x86
+    TRIPLE=x86_64-apple-darwin
+    JAVA_PREBUILT_SUBDIR=mac/Contents/Home
+    OPENJDK_IMAGES_SUBDIR=macosx-x86_64-normal-server-release
+    ;;
+  *) echo "unknown OS:" $(uname) && exit 1;;
 esac
+
+BUILD_TOOLS=${TOP}/prebuilts/build-tools/${PREBUILT_DIR}/bin
 
 ln -sf ${JDK_SRC}/jdk8u/* ${OUT}/
 for i in $(cd ${JDK_SRC}; ls -d jdk8u_*); do
-    rm -f ${OUT}/${i/jdk8u_/}
-    ln -sf ${JDK_SRC}/${i} ${OUT}/${i/jdk8u_/}
+  rm -f ${OUT}/${i/jdk8u_/}
+  ln -sf ${JDK_SRC}/${i} ${OUT}/${i/jdk8u_/}
 done
 
+function configure_openjdk() {
+  (
+    cd $OUT
+    bash configure \
+      --host=$TRIPLE \
+      --disable-zip-debug-info \
+      --with-boot-jdk=${TOP}/prebuilts/studio/jdk/${JAVA_PREBUILT_SUBDIR} \
+      --with-milestone=android \
+      --with-update-version=${JDK_UPDATE_VERSION} \
+      --with-build-number=${JDK_BUILD_NUMBER} \
+      --with-user-release-suffix=${BUILD_NUMBER} \
+      --with-extra-cflags="$GLOBAL_FLAGS" \
+      --with-extra-cxxflags="$GLOBAL_FLAGS" \
+      LDFLAGS="${GLOBAL_FLAGS}" \
+      CFLAGS="${GLOBAL_FLAGS}" \
+      CPPFLAGS="${GLOBAL_FLAGS}" \
+      CXXFLAGS="${GLOBAL_FLAGS}" \
+      X_CFLAGS="${GLOBAL_FLAGS}" \
+      CCXXFLAGS_JDK="${GLOBAL_FLAGS}" \
+      LDFLAGS_JDK="${GLOBAL_FLAGS}" \
+      CC="${CC} ${GLOBAL_FLAGS}" \
+      CXX="${CXX} ${GLOBAL_FLAGS}" \
+      LD="${CC} ${GLOBAL_FLAGS}" \
+      BUILD_CC="$(which ${CC}) ${GLOBAL_FLAGS}" \
+      BUILD_CXX="$(which ${CXX}) ${GLOBAL_FLAGS}" \
+      BUILD_LD="$(which ${CC}) ${GLOBAL_FLAGS}" \
+      ZIP="/usr/bin/zip -X" \
+      "$@"
+  )
+}
+
 function soong_zip() {
-    ${BUILD_TOOLS}/soong_zip -o $1 -C $2 -l <(find $2 | sort)
+  ${BUILD_TOOLS}/soong_zip -o $1 -C $2 -l <(find $2 | sort)
 }
 
 function sanitize_zips() {
-    for i in $(find ${1} -type f \( -name "*.jar" -o -name "*.zip" -o -name "*.sym" \) ); do
-        ${BUILD_TOOLS}/zip2zip -j -t -i $i -o $i.tmp
-        mv -f $i.tmp $i
-    done
+  for i in $(find ${1} -type f \( -name "*.jar" -o -name "*.zip" -o -name "*.sym" \) ); do
+    ${BUILD_TOOLS}/zip2zip -j -t -i $i -o $i.tmp
+    mv -f $i.tmp $i
+  done
+}
+
+function build_openjdk_images() {
+  (
+    cd $OUT
+    make images VERBOSE= ${@}
+    rm -rf images/
+    mkdir images
+    cp -r build/${OPENJDK_IMAGES_SUBDIR}/images/* images/
+    rm -rf images/j2sdk-image/demo/
+    rm -rf images/j2sdk-image/sample/
+    rm -rf images/j2sdk-image/man/
+    if [ -d images/j2sdk-bundle ]; then
+      rm -rf images/j2sdk-bundle/*/Contents/Home/demo/
+      rm -rf images/j2sdk-bundle/*/Contents/Home/sample/
+      rm -rf images/j2sdk-bundle/*/Contents/Home/man/
+    fi
+  )
+
+  sanitize_zips $OUT/images
+
+  if [ -n "${DIST_DIR}" ]; then
+    mkdir -p ${DIST_DIR}
+    DIST=$(cd ${DIST_DIR} && pwd)
+    soong_zip ${DIST}/jre.zip ${OUT}/images/j2re-image
+    soong_zip ${DIST}/jdk.zip ${OUT}/images/j2sdk-image
+    if [ -d "${OUT}/images/j2re-bundle" ]; then
+      soong_zip ${DIST}/jre-bundle.zip ${OUT}/images/j2re-bundle
+      soong_zip ${DIST}/jdk-bundle.zip ${OUT}/images/j2sdk-bundle
+    fi
+    cp -f ${OUT}/build/${OPENJDK_IMAGES_SUBDIR}/config.* ${DIST_DIR}/
+  fi
 }
--- build-openjdk-darwin.sh
+++ build-openjdk-darwin.sh
@@ -1,74 +1,42 @@
 #!/bin/bash
 
-set -e
-
 source $(dirname $0)/build-openjdk-common.sh
 
-freetype=freetype-2.8
-clang_dir=${TOP}/prebuilts/clang/host/darwin-x86/clang-stable
 sdk_version=$(xcrun --show-sdk-version)
 if [ "${sdk_version}" != "10.9" -a "${sdk_version}" != "10.10" ]; then
     echo "Xcode sdk version 10.9 or 10.10 is required"
     exit 1
 fi
 sysroot=$(xcrun --show-sdk-path)
+clang_dir=${TOP}/prebuilts/clang/host/darwin-x86/clang-stable
+freetype=freetype-2.8
 
 export PATH=${clang_dir}/bin:${PATH}
 
 (
   cd ${OUT}
   tar jxf ${TOP}/toolchain/jdk/deps/src/${freetype}.tar.bz2
-  (
-    cd ${freetype}
-    bash configure \
-      CC=clang \
-      CC_BUILD=clang \
-      LD=clang \
-      CFLAGS="-isysroot ${sysroot}" \
-    && make -j8 CCexe_CFLAGS="-isysroot ${sysroot}"
-    mkdir -p lib
-    cp objs/.libs/libfreetype.dylib lib/
-  )
-
-  export MACOSX_DEPLOYMENT_TARGET=10.8
-  bash configure \
-    --host=x86_64-apple-darwin \
-    --with-boot-jdk=${TOP}/prebuilts/studio/jdk/mac/Contents/Home \
-    --with-milestone=android \
-    --with-update-version=${JDK_UPDATE_VERSION} \
-    --with-build-number=${JDK_BUILD_NUMBER} \
-    --with-user-release-suffix=${BUILD_NUMBER} \
-    --with-freetype=${OUT}/${freetype} \
-    --with-extra-cflags="-isysroot ${sysroot}" \
-    --with-extra-cxxflags="-isysroot ${sysroot}" \
-    LDFLAGS="-isysroot ${sysroot}" \
-    CFLAGS="-isysroot ${sysroot}" \
-    CPPFLAGS="-isysroot ${sysroot}" \
-    CXXFLAGS="-isysroot ${sysroot}" \
-    X_CFLAGS="-isysroot ${sysroot}" \
-    CCXXFLAGS_JDK="-isysroot ${sysroot}" \
-    LDFLAGS_JDK="-isysroot ${sysroot}" \
-    CC="clang -isysroot ${sysroot}" \
-    CXX="clang++ -isysroot ${sysroot}" \
-    LD="clang -isysroot ${sysroot}" \
-    BUILD_CC="${clang_dir}/bin/clang -isysroot ${sysroot}" \
-    BUILD_CXX="${clang_dir}/bin/clang++ -isysroot ${sysroot}" \
-    BUILD_LD="${clang_dir}/bin/clang -isysroot ${sysroot}" \
-    ZIP="/usr/bin/zip -X" \
-  && make images VERBOSE= COMPILER_WARNINGS_FATAL=false
 )
 
-build=${OUT}/build/macosx-x86_64-normal-server-release
-images=${build}/images
+GLOBAL_FLAGS="-isysroot ${sysroot}"
+CC=clang
+CXX=clang++
 
-sanitize_zips ${images}
+export MACOSX_DEPLOYMENT_TARGET=10.8
 
-if [ -n "${DIST_DIR}" ]; then
-    mkdir -p ${DIST_DIR}
-    DIST=$(cd ${DIST_DIR} && pwd)
-    soong_zip ${DIST}/jre.zip ${images}/j2re-image
-    soong_zip ${DIST}/jdk.zip ${images}/j2sdk-image
-    soong_zip ${DIST}/jre-bundle.zip ${images}/j2re-bundle
-    soong_zip ${DIST}/jdk-bundle.zip ${images}/j2sdk-bundle
-    cp -f ${build}/config.* ${DIST_DIR}/
-fi
+(
+  cd ${OUT}/${freetype}
+  bash configure \
+    CC=clang \
+    CC_BUILD=clang \
+    LD=clang \
+    CFLAGS="${GLOBAL_FLAGS}" \
+  && make -j8 CCexe_CFLAGS="${GLOBAL_FLAGS}"
+  mkdir -p lib
+  cp objs/.libs/libfreetype.dylib lib/
+)
+
+configure_openjdk \
+  --with-freetype=${OUT}/${freetype}
+
+build_openjdk_images COMPILER_WARNINGS_FATAL=false
