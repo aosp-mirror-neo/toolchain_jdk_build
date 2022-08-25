c5c2e5b0807035f4f3de1299a6fadaa995113c2b
Colin Cross <ccross@android.com>
2022-08-25 22:16:25 +0000

    Revert "Update openjdk builds to clang-r450784e"
    This reverts commit 95640b55acca7d68e9a38ca93b9e35d934419aba.
    
    Reason for revert: aosp/2194897 pinned the manifest to restore clang-416183b, and clang-r450784e doesn't work with OpenJDK11.
    
    Change-Id: I4e9e0712b7e4258a861c0688002c9fd048815ae1

--- build-openjdk11-darwin.sh
+++ build-openjdk11-darwin.sh
@@ -63,7 +63,7 @@ declare -r out_path=$(make_target_dir "$1")
 declare -r sysroot=$(xcrun --show-sdk-path)
 declare -r build_dir="$out_path/build"
 declare -r top=$(realpath "$(dirname "$0")/../../..")
-declare -r clang_bin="$top/prebuilts/clang/host/darwin-x86/clang-r450784e/bin"
+declare -r clang_bin="$top/prebuilts/clang/host/darwin-x86/clang-r416183b/bin"
 declare -r autoconf_dir=$(make_target_dir "$out_path/autoconf")
 
 # So that configure finds tools like dsymutil in ${clang_bin} instead of /usr/bin
