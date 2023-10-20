03f9d0bdcd9edbbed5dd21376890f823339f261e
Eugene Yakavets <yakavets@google.com>
2023-10-20 01:12:44 +0000

    Download wayland from ubuntu 16.04
    Bug: 304625691
    Change-Id: Ice09e751d8a64a332164e9df860479259ef6a59d

--- download-deps-in-docker.sh
+++ download-deps-in-docker.sh
@@ -9,6 +9,12 @@ if [ -z $(docker images -q openjdk-android-deps 2> /dev/null) ]; then
     exit 1
 fi
 
+if [ -z $(docker images -q openjdk-android-deps16 2> /dev/null) ]; then
+    echo "Missing openjdk-android-deps16 image, run:"
+    echo "docker build toolchain/jdk/build -f ./toolchain/jdk/build/Dockerfile.deps16 -t openjdk-android-deps16"
+    exit 1
+fi
+
 docker run \
     --rm \
     -v $(cd toolchain/jdk/build; pwd):/home/build/build:ro \
@@ -18,3 +24,13 @@ docker run \
     -w /home/build/build \
     openjdk-android-deps \
     "./download-deps.sh"
+
+docker run \
+    --rm \
+    -v $(cd toolchain/jdk/build; pwd):/home/build/build:ro \
+    -v $(cd toolchain/jdk/deps; pwd):/home/build/deps \
+    -t \
+    -e USER_ID=$(id -u) \
+    -w /home/build/build \
+    openjdk-android-deps16 \
+    "./download-deps-16.04.sh"
