19f00ea0955302007bad110a60c525b4d68e9335
Eugene Yakavets <yakavets@google.com>
2026-06-15 17:41:43 -0700

    Pin NVDA version for JBR21
    to fix the JBR21 build after ag/40422564
    
    verified with https://android-build.corp.google.com/builds/abtd/run/L07400030133141484
    
    Bug: 519729585
    Test: ABTD
    Change-Id: I7e4f07362933b1778f9b134014697cc516b466e8

--- build-jbr_next-win-x64.sh
+++ build-jbr_next-win-x64.sh
@@ -42,7 +42,7 @@ else
      --without-version-build \
      --with-version-opt="$(numeric_build_number $build_number)-b$jbr_tag" \
      --with-extra-path="/cygdrive/c/tools/cygwin/bin" \
-     --with-nvdacontrollerclient="$top/prebuilts/jdk/studio/nvda/stable" \
+     --with-nvdacontrollerclient="$top/prebuilts/jdk/studio/nvda/2022.4" \
      --enable-cds=yes \
      --disable-ccache \
      --disable-absolute-paths-in-output \
